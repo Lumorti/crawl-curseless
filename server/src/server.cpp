ff3ed42e8c6e744a41624a7203b5b8a89793b33d
Lumorti <lbmortimer1998@gmail.com>
2025-05-31 16:34:52 +0200

    reduced locking

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -10,41 +10,38 @@ std::string fullText = "";
 bp::child c;
 bp::opstream child_stdin;
 bp::ipstream child_stdout;
-bool hasBeenInput = true;
+int numNew = 1;
 
 // C++ entry point
 int main(int argc, const char * argv[]) {
 
     // Disable multithreading
-    svr.new_task_queue = [] { return new httplib::ThreadPool(1); };
+    svr.new_task_queue = [] {return new httplib::ThreadPool(2);};
 
     // Start the process
     c = bp::child("./crawl.exe", bp::std_out > child_stdout, bp::std_in < child_stdin);
 
     // On getting the index, we read as much as we can and output
     svr.Get("/get", [](const httplib::Request &, httplib::Response &res) {
-        std::cout << "Client requested a get" << std::endl;
         fullText = "";
         if (c.running()) {
-            if (hasBeenInput) {
-                std::string line;
-                while (std::getline(child_stdout, line)) {
-                    for (char c : line) {
-                        if (c != '\r' && c != '\n') {
-                            fullText += c;
-                        }
+            if (numNew > 0) {
+                numNew--;
+                char outChar;
+                while (child_stdout.get(outChar)) {
+                    if (outChar != '\r') {
+                        fullText += outChar;
                     }
-                    fullText += '\n';
-                    if (line.size() == 0 || line.find("===READY===") != std::string::npos) {
+                    if (fullText.find("===READY===") != std::string::npos) {
                         break;
                     }
                 }
-                hasBeenInput = false;
             }
         } else {
             fullText = "===CLOSED===";
         }
         res.set_content(fullText, "text/plain");
+        std::cout << "Client requested a get, sent " << fullText.size() << " chars" << std::endl;
     });
 
     // For checking if the server is up
@@ -62,7 +59,7 @@ int main(int argc, const char * argv[]) {
         }
         child_stdin << command << std::endl;
         child_stdin.flush();
-        hasBeenInput = true;
+        numNew++;
         res.set_content("===DONE===", "text/plain");
     });
 
